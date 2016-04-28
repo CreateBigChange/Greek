b7ed286ec99029ad57f3c7b45d0d80623f6e88db
吴辉 <wuhui904107775@qq.com>
2016-04-28 11:46:14 +0800

    修改密码的文档

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "急所需商家管理APP接口",  "version": "1.0.0",  "description": "",  "title": "急所需商家管理app",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-04-26T02:30:59.460Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
+define({  "name": "急所需商家管理APP接口",  "version": "1.0.0",  "description": "",  "title": "急所需商家管理app",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-04-28T03:46:05.297Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
