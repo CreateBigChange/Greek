b6b227ce073233c6eb7f25dab0f8dd3b815d4204
吴辉 <wuhui904107775@qq.com>
2016-05-25 17:17:55 +0800

    更新接口

--- public/apidoc/api_project.js
+++ public/apidoc/api_project.js
@@ -1,1 +1,1 @@
-define({  "name": "急所需商家管理APP接口",  "version": "1.0.0",  "description": "",  "title": "急所需商家管理app",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-05-25T09:09:24.406Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
+define({  "name": "急所需商家管理APP接口",  "version": "1.0.0",  "description": "",  "title": "急所需商家管理app",  "sampleUrl": false,  "apidoc": "0.2.0",  "generator": {    "name": "apidoc",    "time": "2016-05-25T09:17:52.308Z",    "url": "http://apidocjs.com",    "version": "0.15.1"  }});
