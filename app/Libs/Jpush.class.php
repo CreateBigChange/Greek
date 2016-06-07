cbb45cd0a300c063acc35a57270c2b824ce3d71e
吴辉 <wuhui904107775@qq.com>
2016-06-07 16:11:12 +0800

    jpush log

--- app/Libs/Jpush.class.php
+++ app/Libs/Jpush.class.php
@@ -37,7 +37,7 @@ class Jpush
         }
 
         $push->setNotificationAlert('急所需商家版')
-            //->addAllAudience()
+            ->addAllAudience()
             ->addAndroidNotification($content, $title, 1, array("type"=>"new"))
             ->addIosNotification($content, $sound, '+1' , true, 'iOS ORDER NEW', array("type"=>"new"))
             ->setMessage($content, $title, 'type', array("type"=>"new"))
