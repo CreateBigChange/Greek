826a7cc65622bc0496167650dd91bc326fe98e15
吴辉 <wuhui904107775@qq.com>
2016-06-14 09:35:43 +0800

    统计

--- app/Http/Controllers/Gamma/StoresController.php
+++ app/Http/Controllers/Gamma/StoresController.php
@@ -801,8 +801,8 @@ class StoresController extends ApiController
 
         return response()->json(Message::setResponseInfo('SUCCESS' , array(
             'visiting_number'       => $visitingNumber,
-            'order_num'             => $orderCount[0]->order_num,
-            'turnover'              => $orderCount[0]->turnover
+            'order_num'             => $orderCount[0]->order_num ? $orderCount[0]->order_num : 0,
+            'turnover'              => $orderCount[0]->turnover ? $orderCount[0]->turnover : 0
         )));
     }
 }
