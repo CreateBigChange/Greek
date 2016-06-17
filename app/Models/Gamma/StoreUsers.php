06b7a11f7c1fb03951b082396d0bf652f42753c9
吴辉 <wuhui904107775@qq.com>
2016-06-17 18:32:30 +0800

    提现

--- app/Models/Gamma/StoreUsers.php
+++ app/Models/Gamma/StoreUsers.php
@@ -220,7 +220,7 @@ class StoreUsers extends Model
         foreach ($result as $r){
 
             ///(^.*)\d{4}(\d{4})$/
-            $r->bank_card_num = preg_replace('/(\d{4})$/','\\1',$r->bank_card_num);
+            $r->bank_card_num = preg_replace('/(\d{4})$/','\\2',$r->bank_card_num);
             //$r->bank_card_num = substr_replace($r->bank_card_num, '', -1 , 4);
         }
 
