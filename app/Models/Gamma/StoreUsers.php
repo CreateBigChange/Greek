f082099c028d6faa628e15c3c8b0bbf5fbe5ac3d
吴辉 <wuhui904107775@qq.com>
2016-06-23 09:17:10 +0800

    可提现

--- app/Models/Gamma/StoreUsers.php
+++ app/Models/Gamma/StoreUsers.php
@@ -78,7 +78,10 @@ class StoreUsers extends Model
                       sc.is_close,
                       sc.bell,
                       sc.point,
-                      sc.money
+                      sc.money,
+                      si.provice,
+                      si.city,
+                      si.county
                 FROM $this->_table AS su";
         $sql .= " LEFT JOIN store_infos as si ON su.store_id = si.id";
         $sql .= " LEFT JOIN store_configs as sc ON su.store_id = sc.store_id";
