7f1dbbf599f3346e146214f6fbb7b1e81bbe3cbb
吴辉 <wuhui904107775@qq.com>
2016-06-25 16:55:04 +0800

    支付宝退款

--- config/alipay.php
+++ config/alipay.php
@@ -5,7 +5,7 @@ return [
     'seller_user_id'        => 'zxhy201510@163.com',
     'private_key_path'      => public_path() . '/alipay/rsa_private_key.pem',
     'ali_public_key_path'   => public_path().'/alipay/alipay_public_key.pem',
-    'notify_url'            => 'http://preview.jisxu.com/sigma/alipay/notify/refund',
+    'notify_url'            => 'http://preview.jisxu.com/alipay/notify/refund',
     'sign_type'             => strtoupper('RSA'),
     'refund_date'           => date("Y-m-d H:i:s",time()),
     'input_charset'         => strtolower('utf-8'),
