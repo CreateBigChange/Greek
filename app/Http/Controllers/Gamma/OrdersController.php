52838e7820b43b89d2aad5c765fbc495a6ad25cd
吴辉 <wuhui904107775@qq.com>
2016-06-25 15:40:31 +0800

    支付宝退款

--- app/Http/Controllers/Gamma/OrdersController.php
+++ app/Http/Controllers/Gamma/OrdersController.php
@@ -237,10 +237,10 @@ class OrdersController extends ApiController
         //构造要请求的参数数组，无需改动
         header("Content-type:text/html;charset=utf-8");
         $parameter = array(
-            "service"           => 'refund_fastpay_by_platform_nopwd',
+            "service"           => trim(Config::get('alipay.refund_service')),
             "partner"           => trim(Config::get('alipay.partner')),
-            "notify_url"	    => 'http://preview.jisxu.com',
-            "refund_date"	    => date('Y-m-d H:i:s' , time()),
+            "notify_url"	    => trim(Config::get('alipay.notify_url')),
+            "refund_date"	    => trim(Config::get('alipay.refund_date')),
             "batch_no"	        => date('YmdHis' , time()) . $this->getSalt(4, 1),
             "batch_num"	        => 1,
             "detail_data"	    => $orderNo.'^'.$payTotal.'^'.'正常退款',
