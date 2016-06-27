09fb850de1ebd8aa9fd6121e6b39c10f8b33e206
吴辉 <wuhui904107775@qq.com>
2016-06-27 10:28:32 +0800

    评价

--- app/Models/OrderEvaluate.php
+++ app/Models/OrderEvaluate.php
@@ -11,6 +11,7 @@ use DB , Config;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Models\Order;
+use App\Models\OrderLog;
 
 class OrderEvaluate extends Model{
 
@@ -23,11 +24,12 @@ class OrderEvaluate extends Model{
      */
     public function evaluate($data){
         
-        $orderModel = new Order();
+        $orderModel     = new Order();
+        $orderLogModel  = new OrderLog();
         
         if(DB::table($this->table)->insert($data)){
             DB::table($orderModel->getTable())->where('id' , $data['order_id'])->update(array('is_evaluate' => 1 , 'status' => 1));
-            $this->createOrderLog($data['order_id'], $data['user_id'], '普通用户', '用户端APP', '订单评价完成' , Config::get('orderstatus.completd')['status']);
+            $orderLogModel->createOrderLog($data['order_id'], $data['user_id'], '普通用户', '用户端APP', '订单评价完成' , Config::get('orderstatus.completd')['status']);
             return true;
         }else{
             return false;
