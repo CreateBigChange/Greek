83987405f73c18789868b547bb8b237c6a9b2422
吴辉 <wuhui904107775@qq.com>
2016-06-27 14:30:03 +0800

    上传图片到七牛

--- scripts/uploadQiniu.php
+++ scripts/uploadQiniu.php
@@ -53,13 +53,15 @@ while ($row = $goodsResult->fetch_object()) {
         return response()->json( Message::setResponseInfo( 'FAILED' ) );
     } else {
         $sql = "UPDATE goods SET `img`=" . "http://7xt4zt.com2.z0.glb.clouddn.com/" . $ret['key'] . " WHERE `id` = " . $row->id;
+        var_dump($sql);die;
         $mysqli->query($sql);
+
+        die;
     }
+
 }
 
 
-
-die;
 
 
 
