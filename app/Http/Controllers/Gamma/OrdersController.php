0a79abbbe7ffce95adc3607c02914609611c4417
吴辉 <wuhui904107775@qq.com>
2016-06-28 18:06:43 +0800

    推送的数量

--- app/Http/Controllers/Gamma/OrdersController.php
+++ app/Http/Controllers/Gamma/OrdersController.php
@@ -489,7 +489,7 @@ class OrdersController extends ApiController
     public function getOrderNum(){
         $storeId = $this->storeId;
 
-        var_dump( Redis::get("store:$storeId:new"));
+        var_dump( Redis::get("store:$storeId:new"));die;
         $data = array();
         $data['new'] = Redis::get("store:$storeId:new") == null ? 0 : Redis::get("store:$storeId:new");
         $data['accident'] = Redis::get("store:$storeId:accident") == null ? 0 : Redis::get("store:$storeId:accident");
