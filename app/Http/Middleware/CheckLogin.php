9e288b12dcce44ca1460fdba380b6333656297e1
吴辉 <wuhui904107775@qq.com>
2016-06-30 10:38:06 +0800

    栏目排序

--- app/Http/Middleware/CheckLogin.php
+++ app/Http/Middleware/CheckLogin.php
@@ -31,7 +31,7 @@ class CheckLogin
         }
 
 
-        BLogger::getLogger(BLogger::LOG_WECHAT_PAY)->notice(get_headers("http://preview.jisxu.com"));
+        BLogger::getLogger(BLogger::LOG_WECHAT_PAY)->notice(getallheaders());
 
         return $next($request);
     }
