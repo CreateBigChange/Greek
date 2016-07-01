89957824e033b696888626684015b15a172ca25c
吴辉 <wuhui904107775@qq.com>
2016-07-01 11:53:26 +0800

    高德标记

--- scripts/amap.php
+++ scripts/amap.php
@@ -19,8 +19,8 @@ function amap()
         "loctype" => "1",
     );
 
-    //$mysqli = new mysqli('rm-wz9s022vq140vwejy.mysql.rds.aliyuncs.com' , 'zxshop' , 'zxhy-2016' , 'zxshop');
-    $mysqli = new mysqli('127.0.0.1' , 'root' , '123456' , 'zxshop');
+    $mysqli = new mysqli('rm-wz9s022vq140vwejy.mysql.rds.aliyuncs.com' , 'zxshop' , 'zxhy-2016' , 'zxshop');
+    //$mysqli = new mysqli('127.0.0.1' , 'root' , '123456' , 'zxshop');
     if ($mysqli->connect_error) {
         die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
     }
