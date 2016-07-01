bbaeac8f2df4f35cb77d2b5bc2b342b9d733e00e
吴辉 <wuhui904107775@qq.com>
2016-07-01 12:07:09 +0800

    高德标记

--- scripts/amap.php
+++ scripts/amap.php
@@ -5,7 +5,7 @@ function amap()
     $config = [
 
         "key" => "c8149a3da34eaa5a1de69a34e1a42412",
-        "tableid" => "5739ae52305a2a1894e73386",
+        "tableid" => "573984757bbf1905ead016e3",
         "url" => "http://yuntuapi.amap.com/datamanage/data/create",
         "geo_url" => "http://restapi.amap.com/v3/geocode/geo",
         "regeo_url" => "http://restapi.amap.com/v3/geocode/regeo",
