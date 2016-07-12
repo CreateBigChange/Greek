379a58307a91f0bc7d367480f0f42d4f61706081
吴辉 <wuhui904107775@qq.com>
2016-07-12 17:04:48 +0800

    脚本

---
+++ scripts/updateJsxStoreLogo.php
@@ -0,0 +1,27 @@
+<?php
+
+$mysqli = new mysqli('rm-wz9s022vq140vwejy.mysql.rds.aliyuncs.com' , 'zxshop' , 'zxhy-2016' , 'zxshop');
+
+$mysqliJsx = new mysqli('rm-wz9s022vq140vwejy.mysql.rds.aliyuncs.com' , 'jsx' , '*pzsJqbd^6rvTeuz' , 'jsx');
+//$mysqli = new mysqli('192.168.0.249' , 'root' , '123456' , 'zxshop');
+if ($mysqli->connect_error) {
+    die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
+}
+
+
+
+$mysqli->query("set names utf8");
+
+$zxshopSql = "SELECT * FROM store_configs";
+
+$zxshopResult = $mysqli->query($zxshopSql);
+
+while ($row = $zxshopResult->fetch_row()){
+
+    $jsxSql = "UPDATE `store_configs` SET `store_logo` = " . $row->store_logo . "WHERE `id` = " .$row->store_id;
+    $mysqliJsx->query($jsxSql);
+}
+
+$mysqli->close();
+
+?>
