7b8486b8228afbadacbdbcdfad8e2d2b78038cf2
吴辉 <wuhui904107775@qq.com>
2016-07-12 10:13:52 +0800

    cookie 过期时间

--- config/session.php
+++ config/session.php
@@ -153,11 +153,11 @@ return [
     'web_login_cookie' => 'jisux_store_admin',
     'store_app_login_cookie' => 'jisux_store_app',
     //一年
-    'store_app_lifetime' => 315360000000,
+    'store_app_lifetime' => 315360000,
 
     'sigma_login_cookie' => 'jisux_app',
 
     //一年
-    'sigma_lifetime' => 315360000000,
+    'sigma_lifetime' => 315360000,
 
 ];
