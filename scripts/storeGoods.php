78f98648b526d5d636801810e02006a3e590f520
吴辉 <wuhui904107775@qq.com>
2016-07-13 09:15:51 +0800

    脚本

--- scripts/storeGoods.php
+++ scripts/storeGoods.php
@@ -15,18 +15,18 @@ require_once 'PHPExcel/Classes/PHPExcel/IOFactory.php';
 
 
 // Check prerequisites
-if (!file_exists("storeGoodsData.xls")) {
-    exit("not found storeGoodsData.xls.\n");
+if (!file_exists("storeGoods.xls")) {
+    exit("not found storeGoods.xls.\n");
 }
 
 $reader = PHPExcel_IOFactory::createReader('Excel5'); //设置以Excel5格式(Excel97-2003工作簿)
-$PHPExcel = $reader->load("storeGoodsData.xls"); // 载入excel文件
+$PHPExcel = $reader->load("storeGoods.xls"); // 载入excel文件
 $sheet = $PHPExcel->getSheet(0); // 读取第一個工作表
 $highestRow = $sheet->getHighestRow(); // 取得总行数
 $highestColumm = $sheet->getHighestColumn(); // 取得总列数
 //
-//$mysqli = new mysqli('rm-wz9s022vq140vwejy.mysql.rds.aliyuncs.com' , 'zxshop' , 'zxhy-2016' , 'zxshop');
-$mysqli = new mysqli('192.168.0.249' , 'root' , '123456' , 'zxshop');
+$mysqli = new mysqli('rm-wz9s022vq140vwejy.mysql.rds.aliyuncs.com' , 'zxshop' , 'zxhy-2016' , 'zxshop');
+//$mysqli = new mysqli('192.168.0.249' , 'root' , '123456' , 'zxshop');
 if ($mysqli->connect_error) {
     die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
 }
@@ -69,39 +69,59 @@ for ($row = 2; $row <= $highestRow; $row++){//行数是以第1行开始
 
     for ($column = 'B'; $column <= $highestColumm; $column++) {
 
+        $value = trim($sheet->getCell($column.$row)->getValue());
         if($column == 'B'){
-            $storeGoods[$storeId][$length]['name'] = trim($sheet->getCell($column.$row)->getValue());
-            $storeGoods[$storeId][$length]['desc'] = trim($sheet->getCell($column.$row)->getValue());
+            $name = $value;
+            $desc = $value;
         }
         if($column == 'C'){
-            $storeGoods[$storeId][$length]['spec'] = trim($sheet->getCell($column.$row)->getValue());
+            $selectBrand = "SELECT * FROM goods_brand WHERE `name` = '" . $value ."'";
+            $selectBrandResult = $mysqli->query($selectBrand);
+            $brand = $selectBrandResult->fetch_object();
+            if($brand) {
+                $b_id = $brand->id;
+            }
         }
         if($column == 'D'){
-            $storeGoods[$storeId][$length]['img'] = trim($sheet->getCell($column.$row)->getValue());
+            $selectCategory = "SELECT * FROM goods_categories WHERE `name` = '" . $value ."'";
+            $selectCategoryResult = $mysqli->query($selectCategory);
+            $category = $selectCategoryResult->fetch_object();
+            if($category) {
+                $c_id = $category->id;
+            }
+
+            $selectNav = "SELECT * FROM store_nav WHERE `name` = '" . $value ."'";
+            $selectNavResult = $mysqli->query($selectNav);
+            $nav = $selectNavResult->fetch_object();
+            if($nav) {
+                $nav_id = $nav->id;
+            }
+
         }
-        if($column == 'J'){
-            $storeGoods[$storeId][$length]['price'] = trim($sheet->getCell($column.$row)->getValue());
+        if($column == 'E'){
+            $spec = $value;
+        }
+        if($column == 'F'){
+            $img = $value;
+        }
+        if($column == 'L'){
+            $price = $value;
         }
 
     }
+    $sql            = "INSERT INTO store_goods(`store_id` , `c_id` , `nav_id` , `b_id` , `goods_id` , `name` , `out_price` , `img` , `spec` ,  `desc`) VALUES ($storeId" ."," . $c_id . "," . $nav_id . "," . $b_id . "," . 0 . ",'" . $name. "', " . $price . ",'" . $img . "','" . $spec . "','" . $desc . "')";
+    $result         = $mysqli->query($sql);
 
-}
-
-
-foreach ($storeGoods as $sgK => $sgV) {
-
-    foreach ($sgV as $gv){
-        $sql            = "INSERT INTO store_goods(`store_id` , `goods_id` , `name` , `out_price` , `img` , `spec` ,  `desc`) VALUES ($sgK" ."," . 0 . ",'" . $gv['name']. "', " . $gv['price'] . ",'" . $gv['img'] . "','" . $gv['spec'] . "','" . $gv['desc'] . "')";
-        $result         = $mysqli->query($sql);
-
-        if($result){
-            echo "成功插入一条,店铺名称:".$gv['name']."\n";
-        }else{
-            echo "失败一条,店铺名称:" . $gv['name']."\n";
-        }
+    if($result){
+        echo "成功插入一条,商品名称:".$name."\n";
+    }else{
+        echo "失败一条,商品名称:" . $name."\n";
     }
 
+
+
 }
+
 
 $mysqli->close();
 
