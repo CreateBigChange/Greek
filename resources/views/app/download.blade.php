5b9ca13f1478ff7ae3768e8f58a0c6de66c170bb
吴辉 <wuhui904107775@qq.com>
2016-07-19 10:24:52 +0800

    测试

--- resources/views/app/download.blade.php
+++ resources/views/app/download.blade.php
@@ -54,7 +54,22 @@
                 location.href = "{{ $download }}"
             }
         @elseif ($system == 'ios')
+            var winHeight = $(window).height();
+            function is_weixin() {
+                var ua = navigator.userAgent.toLowerCase();
+                if (ua.match(/MicroMessenger/i) == "micromessenger") {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+            var isWeixin = is_weixin();
+            if(isWeixin){
+                $(".weixin-tip").css("height",winHeight);
+                $(".weixin-tip").show();
+            }else {
                 location.href = "{{ $download }}"
+            }
         @endif
     })
 
