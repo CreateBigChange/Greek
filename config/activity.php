8fb91c15af860c61269ffb550c6efd6a83d48035
吴辉 <wuhui904107775@qq.com>
2016-07-20 20:06:16 +0800

    优惠卷

--- config/activity.php
+++ config/activity.php
@@ -2,6 +2,6 @@
 
 return [
     //首次注册赠送的优惠券ID
-    'first_register_give_coupon_id'             => 5,
+    'first_register_give_coupon_id'             => 4,
     'first_register_give_coupon_number'         => 3,
 ];
