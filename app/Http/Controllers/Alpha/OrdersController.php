c9334b6b6315ac4874e32b4aa25a8c5b607636d5
吴辉 <wuhui904107775@qq.com>
2016-07-26 19:09:53 +0800

    订单分页

--- app/Http/Controllers/Alpha/OrdersController.php
+++ app/Http/Controllers/Alpha/OrdersController.php
@@ -150,7 +150,7 @@ class OrdersController extends AdminController
 
         $this->response['page']         = $pageData->page;
         $this->response['pageData']     = $pageData;
-        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/goods?' );
+        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/notdelivery?' );
         $this->response['orders']       = $this->_model->getOrderList($search , $this->length , $pageData->offset);
         $this->response['status']       = $status;
 
@@ -183,7 +183,7 @@ class OrdersController extends AdminController
 
         $this->response['page']         = $pageData->page;
         $this->response['pageData']     = $pageData;
-        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/goods?' );
+        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/delivery?' );
         $this->response['orders']       = $this->_model->getOrderList($search , $this->length , $pageData->offset);
         $this->response['status']       = $status;
 
@@ -227,7 +227,7 @@ class OrdersController extends AdminController
 
         $this->response['page']         = $pageData->page;
         $this->response['pageData']     = $pageData;
-        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/goods?' );
+        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/accident?' );
         $this->response['orders']       = $this->_model->getOrderList($search , $this->length , $pageData->offset);
         $this->response['status']       = $status;
 
@@ -287,7 +287,7 @@ class OrdersController extends AdminController
 
         $this->response['page']         = $pageData->page;
         $this->response['pageData']     = $pageData;
-        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/delivery?' );
+        $this->response['pageHtml']     = $this->getPageHtml($pageData->page , $pageData->totalPage  , '/alpha/dispatching?' );
         $this->response['orders']       = $this->_model->getOrderList($search , $this->length , $pageData->offset);
         $this->response['status']       = $status;
         return view('alpha.order.list' , $this->response);
