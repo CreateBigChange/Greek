0c35559680fe4ef0794d2df9c0a1143a37cb4d82
yangxiansheng <312479274@qq.com>
2016-07-27 10:09:15 +0800

    订单修改bug

--- resources/views/alpha/order/list.blade.php
+++ resources/views/alpha/order/list.blade.php
@@ -18,12 +18,10 @@
                  <thead>
                     <tr>
                       <th>收货人</th>
-
                       <th>收货地址</th>
                       <th>店铺</th>
                       <th>状态</th>
                       <th>订单号</th>
-
                       <th>支付类型</th>
                       <th>配送费</th>
                       <th>时间</th>
@@ -31,14 +29,13 @@
                       <th>优惠券价格</th>
                       <th>扣点数</th>
                       <th>订单总价</th>
-                       <th>操作</th>
+                      <th>操作</th>
                     </tr>
                  </thead>
                  <tbody>
                   @foreach ($orders  as $order)
                       <tr class="table" >
                          <td>{{$order->consignee}}</td>
-
                          <td>{{$order->consignee_address}}</td>
                          <td>{{$order->sname}}</td>
                          <td>
@@ -75,7 +72,6 @@
                           ?>
                          </td>
                          <td>{{$order->order_num}}</td>
-
                          <td>{{$order->pay_type_name }}</td>
                          <td>{{$order->deliver}}</td>
                          <td>{{$order->created_at}}</td>
@@ -86,24 +82,21 @@
                                  case 0: echo "平台券";
                                      break;
                                  default:break;
-                         }
+                             }
                              ?>
                          </td>
                          <td>{{$order->coupon_actual_reduce}}</td>
                          <td>{{$order->money_reduce_points}}</td>
                          <td>{{$order->total}}</td>
-                          <td><button class="btn btn-success detail_button" data-toggle="modal" href="#detail"
+                         <td><button class="btn btn-success detail_button" data-toggle="modal" href="#detail"
 
                               other_consignee =  "{{$order->consignee}}"         other_id =  '{{$order->id}}'             other_consignee_tel =  '{{$order->consignee_tel}}'
                               other_refund_reason ='{{$order->refund_reason}}'   other_mobile ='{{$order->mobile}}'       other_ture_name = '{{$order->true_name}}'
                               other_store_income = '{{$order->store_income}}'    other_trade_no = '{{$order->trade_no}}'  other_updated_at='{{$order->updated_at}}'
                               other_city = '{{$order->city}}'                    other_street = '{{$order->street}}'      other_country = '{{$order->county}}'
                               other_deliver= '{{$order->deliver}}'               other_remark = '{{$order->remark}}'      other_transaction_id = '{{$order->transaction_id}}'
-                              other_trade_no = '{{$order->trade_no}}'
-
-                              ><i class="icon-search"
-
-                           >详情</button></td>
+                              other_trade_no = '{{$order->trade_no}}'>
+                         <i class="icon-search">详情</button></td>
                      </tr>
                   @endforeach
                  </tbody>
@@ -115,35 +108,30 @@
                 </section>
             </div>
         </div>
-
     </section>
-
-
 </section>
 <!--main content end-->
-
 <script>
 
     $(".detail_button").click(function () {
 
-        $("#other_city").attr("value",$(this).attr("other_city"));
-        $("#other_consignee").attr("value",$(this).attr("other_consignee"));
-        $("#other_consignee_tel").attr("value",$(this).attr("other_consignee_tel"));
-        $("#other_deliver").attr("value",$(this).attr("other_deliver"));
-        $("#other_updated_at").attr("value",$(this).attr("other_updated_at"));
-        $("#other_country").attr("value",$(this).attr("other_country"));
-        $("#other_id").attr("value",$(this).attr("other_id"));
-        $("#other_mobile").attr("value",$(this).attr("other_mobile"));
-        $("#other_refund_reason").attr("value",$(this).attr("other_refund_reason"));
-        $("#other_remark").attr("value",$(this).attr("other_remark"));
-        $("#other_store_income").attr("value",$(this).attr("other_store_income"));
-        $("#other_street").attr("value",$(this).attr("other_street"));
-        $("#other_trade_no").attr("value",$(this).attr("other_trade_no"));
-        $("#other_transaction_id").attr("value",$(this).attr("other_transaction_id"));
-        $("#other_true_name").attr("value",$(this).attr("other_true_name"));
+            $("#other_city").attr("value",$(this).attr("other_city"));
+            $("#other_consignee").attr("value",$(this).attr("other_consignee"));
+            $("#other_consignee_tel").attr("value",$(this).attr("other_consignee_tel"));
+            $("#other_deliver").attr("value",$(this).attr("other_deliver"));
+            $("#other_updated_at").attr("value",$(this).attr("other_updated_at"));
+            $("#other_country").attr("value",$(this).attr("other_country"));
+            $("#other_id").attr("value",$(this).attr("other_id"));
+            $("#other_mobile").attr("value",$(this).attr("other_mobile"));
+            $("#other_refund_reason").attr("value",$(this).attr("other_refund_reason"));
+            $("#other_remark").attr("value",$(this).attr("other_remark"));
+            $("#other_store_income").attr("value",$(this).attr("other_store_income"));
+            $("#other_street").attr("value",$(this).attr("other_street"));
+            $("#other_trade_no").attr("value",$(this).attr("other_trade_no"));
+            $("#other_transaction_id").attr("value",$(this).attr("other_transaction_id"));
+            $("#other_true_name").attr("value",$(this).attr("other_true_name"));
 
     });
-
     var table = $(".table");
     var i=0;
     $("#table .table").each(function(){
@@ -157,7 +145,6 @@
          $(this).addClass("active");
      i++;
     });
-
     $('.showOrderGoods').bind('click' , function() {
         var _this = this;
         var orderId = $(_this).attr('orderId');
